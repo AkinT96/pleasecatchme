0abfcecafee5ecc1a2e6aa0208e6be550daa5c65
akin- <akin-tas96@hotmail.de>
2025-02-11 01:06:21 +0100

    first version

--- tile_01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tile_01" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="tiles/PNG/Tiles/tile_01.png" width="64" height="64"/>
-</tileset>
--- tile_427.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tile_427" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="tiles/PNG/Tiles/tile_427.png" width="64" height="64"/>
-</tileset>
